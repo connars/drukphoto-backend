4cf4565444880d0c057861c2b2c7230e2cf67a1d
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-02-19 02:08:57 +0200

    add

--- index.js
+++ index.js
@@ -8,29 +8,75 @@ const port = 1228;
 const fs = require('fs');
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, file.originalname);
-    }
-  });
-  
-  const upload = multer({ storage: storage });
-  
-  app.post('/upload', upload.array('photos'), function (req, res) {
+  destination: function(req, res, cb) {
+      const date = new Date().toISOString().slice(0, 10);
+      const dir = `uploads/${date}/`;
+      const phoneDir = `${dir}folder/`;
+      fs.mkdir(phoneDir, { recursive: true }, function (err) {
+        if (err) {
+          console.log(err);
+          cb(err, null);
+        } else {
+          cb(null, phoneDir);
+        }
+      });
+  },
+  filename: function(req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+app.post('/upload', upload.array('photos'), function (req, res) {
+    const phone = req.body.phone;
+    console.log(phone);
     const files = req.files;
     const filePaths = files.map(file => `${file.destination}${file.filename}`);
     Promise.all(
       files.map(file => fs.promises.rename(file.path, `${file.destination}${file.filename}`))
     ).then(() => {
-      res.send('Files uploaded successfully');
+      const { name, surname,  email} = req.body;
+      if (!phone) {
+        res.status(400).send('Phone number is missing in the request body');
+        return;
+      }
+      const photoData = files.map(file => ({
+        filename: file.originalname,
+        mimetype: file.mimetype,
+        size: file.size
+      }));
+     
+      console.log(`Name: ${name}, Surname: ${surname}, Email: ${email}, Phone: ${phone}`);
+      console.log(photoData);
+
+      const date = new Date().toISOString().slice(0, 10);
+      const dir = `uploads/${date}/`;
+      const oldPhoneDir = `${dir}folder/`;
+      const newPhoneDir = `${dir}${phone}/`;
+
+      if (fs.existsSync(newPhoneDir)) {
+        console.log(`Folder ${newPhoneDir} already exists`);
+        res.send('Files uploaded successfully');
+        return;
+      }
+
+      fs.rename(oldPhoneDir, newPhoneDir, function(err) {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error renaming folder');
+        } else {
+          console.log(`Folder renamed to ${phone}`);
+          res.send('Files uploaded successfully');
+        }
+      });
+
     }).catch(err => {
       console.log(err);
       res.status(500).send('Error uploading files');
     });
-  });
+});
+
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
