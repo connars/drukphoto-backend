4e4726b2ec605a8ec7bf357d983f7d5d76ffb346
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-02-19 02:23:37 +0200

    add

--- index.js
+++ index.js
@@ -8,73 +8,42 @@ const port = 1228;
 const fs = require('fs');
 
 const storage = multer.diskStorage({
-  destination: function(req, res, cb) {
-      const date = new Date().toISOString().slice(0, 10);
-      const dir = `uploads/${date}/`;
-      const phoneDir = `${dir}folder/`;
-      fs.mkdir(phoneDir, { recursive: true }, function (err) {
-        if (err) {
-          console.log(err);
-          cb(err, null);
-        } else {
-          cb(null, phoneDir);
-        }
-      });
+  destination: function(req, file, cb) {
+    cb(null, 'temp/');
   },
   filename: function(req, file, cb) {
-    const ext = path.extname(file.originalname);
     cb(null, file.originalname);
   }
 });
 
 const upload = multer({ storage: storage });
-app.post('/upload', upload.array('photos'), function (req, res) {
-    const phone = req.body.phone;
-    console.log(phone);
-    const files = req.files;
-    const filePaths = files.map(file => `${file.destination}${file.filename}`);
-    Promise.all(
-      files.map(file => fs.promises.rename(file.path, `${file.destination}${file.filename}`))
-    ).then(() => {
-      const { name, surname,  email} = req.body;
-      if (!phone) {
-        res.status(400).send('Phone number is missing in the request body');
-        return;
-      }
-      const photoData = files.map(file => ({
-        filename: file.originalname,
-        mimetype: file.mimetype,
-        size: file.size
-      }));
-     
-      console.log(`Name: ${name}, Surname: ${surname}, Email: ${email}, Phone: ${phone}`);
-      console.log(photoData);
 
-      const date = new Date().toISOString().slice(0, 10);
-      const dir = `uploads/${date}/`;
-      const oldPhoneDir = `${dir}folder/`;
-      const newPhoneDir = `${dir}${phone}/`;
+app.post('/upload', upload.array('photos'), function(req, res) {
+  const phone = req.body.phone;
+  const { name, surname, email } = req.body;
+  if (!phone) {
+    res.status(400).send('Phone number is missing in the request body');
+    return;
+  }
 
-      if (fs.existsSync(newPhoneDir)) {
-        console.log(`Folder ${newPhoneDir} already exists`);
-        res.send('Files uploaded successfully');
-        return;
-      }
+  const date = new Date().toISOString().slice(0, 10);
+  const dir = `uploads/${date}/${phone}/`;
 
-      fs.rename(oldPhoneDir, newPhoneDir, function(err) {
-        if (err) {
-          console.log(err);
-          res.status(500).send('Error renaming folder');
-        } else {
-          console.log(`Folder renamed to ${phone}`);
-          res.send('Files uploaded successfully');
-        }
-      });
+  fs.mkdirSync(dir, { recursive: true });
 
-    }).catch(err => {
-      console.log(err);
-      res.status(500).send('Error uploading files');
-    });
+  const promises = req.files.map(file => {
+    const oldPath = `temp/${file.filename}`;
+    const newPath = `${dir}/${file.originalname}`;
+    return fs.promises.rename(oldPath, newPath);
+  });
+
+  Promise.all(promises).then(() => {
+    console.log(`Name: ${name}, Surname: ${surname}, Email: ${email}, Phone: ${phone}`);
+    res.send('Files uploaded successfully');
+  }).catch(err => {
+    console.error(err);
+    res.status(500).send('Error uploading files');
+  });
 });
 
 const server = http.createServer(app);
