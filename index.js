f38da25e295af5b33f89ebf767e178e3ca37a122
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2023-02-26 16:16:16 +0500

    added

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const http = require('http');
-const path = require('path');
-const express = require('express');
-const multer = require('multer');
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const multer = require("multer");
 const app = express();
-const hostname = '127.0.0.1';
-const port = 1228;
-const fs = require('fs');
+app.use(express.json());
+const port = process.env.PORT || 3000;
+const fs = require("fs");
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'temp/');
+  destination: function (req, file, cb) {
+    cb(null, "temp/");
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     cb(null, file.originalname);
-  }
+  },
 });
 
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.array('photos'), function(req, res) {
+app.post("/upload", upload.array("photos"), function (req, res) {
   const phone = req.body.phone;
   const { name, surname, email } = req.body;
   if (!phone) {
-    res.status(400).send('Phone number is missing in the request body');
+    res.status(400).send("Phone number is missing in the request body");
     return;
   }
 
@@ -31,25 +31,81 @@ app.post('/upload', upload.array('photos'), function(req, res) {
 
   fs.mkdirSync(dir, { recursive: true });
 
-  const promises = req.files.map(file => {
- 
+  const promises = req.files.map((file) => {
     const oldPath = `temp/${file.filename}`;
     const newPath = `${dir}/${file.originalname}`;
     // const newPath = path.join(dir, file.originalname);
     return fs.promises.rename(oldPath, newPath);
   });
   console.log(promises);
-  Promise.all(promises).then(() => {
-    console.log(`Name: ${name}, Surname: ${surname}, Email: ${email}, Phone: ${phone}`);
-    res.send('Files uploaded successfully');
-  }).catch(err => {
-    console.error(err);
-    res.status(500).send('Error uploading files');
-  });
+  Promise.all(promises)
+    .then(() => {
+      console.log(
+        `Name: ${name}, Surname: ${surname}, Email: ${email}, Phone: ${phone}`
+      );
+      res.send("Files uploaded successfully");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error uploading files");
+    });
 });
 
-const server = http.createServer(app);
+app.post("/crm-add", async (req, res) => {
+  try {
+    let { name, surname, mail, phone, price, delivery, adress } = req.body;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    if (!(name && surname && mail && phone && price && delivery && adress)) {
+      throw Error("void data");
+    }
+
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Cookie", "_csrf=SLYAcwTTqVHsCmG-Sy8pg13Zj-CiYPAN");
+
+    let raw = JSON.stringify({
+      form: "DKooe-JJggzbJC-sfXadyfYJdFk3r9eyulTnIE7yeI8JyJf7dHeEJjToaemPWwmiv2sdJp",
+      getResultData: "1",
+      fName: name,
+      lName: surname,
+      phone: phone,
+      email: mail,
+      products: [
+        {
+          id: "1",
+          name: "Фотография",
+          costPerItem: price,
+          amount: price,
+          description: "Фотография красивая",
+        },
+      ],
+      payment_method: "Карта",
+      shipping_method: delivery,
+      shipping_address: adress,
+    });
+
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    let data = await fetch(
+      "https://fotka.salesdrive.me/handler/",
+      requestOptions
+    );
+    data = await data.json();
+
+    res.status(200).json({
+      status: 200,
+      message: data,
+    });
+  } catch (error) {
+    res.status(400).json({ status: 400, message: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server start ${port}`);
 });
