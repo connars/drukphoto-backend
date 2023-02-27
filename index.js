bb9c74d2dd2c8f70c4d8772883e3514f464d482e
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2023-02-27 23:19:48 +0500

    added

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const multer = require("multer");
 import cors from "cors";
 const app = express();
 app.use(cors());
+app.use(function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+});
 app.use(express.json());
 const port = process.env.PORT || 3000;
 const fs = require("fs");
